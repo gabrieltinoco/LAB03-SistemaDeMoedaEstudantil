a38a417d70b07cdbdd66e061f9d722187cb67777
Gabriel L Tinoco <gabriel.taguiar@gmail.com>
2025-05-13 15:36:22 -0300

    Services

--- campuscash/campuscash/src/main/java/com/laboratorio03/campuscash/controllers/AlunoController.java
+++ campuscash/campuscash/src/main/java/com/laboratorio03/campuscash/controllers/AlunoController.java
@@ -3,6 +3,7 @@ package com.laboratorio03.campuscash.controllers;
 import com.laboratorio03.campuscash.dto.AlunoDTO;
 import com.laboratorio03.campuscash.models.Aluno;
 import com.laboratorio03.campuscash.repositories.AlunoRepository;
+import com.laboratorio03.campuscash.services.AlunoService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.EmptyResultDataAccessException;
@@ -14,125 +15,51 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
-import java.util.List;
-import java.util.Optional;
 
 @Controller
 @RequestMapping("/alunos")
 public class AlunoController {
 
     @Autowired
-    private AlunoRepository alunoRepository;
+    AlunoService alunoService;
 
     @GetMapping("")
     public ModelAndView index () {
-
-        List<Aluno> alunos = this.alunoRepository.findAll();
-        ModelAndView mv = new ModelAndView("alunos/index");
-        mv.addObject("alunos", alunos);
-        mv.addObject("page", "alunos");
-        return mv;
+        return alunoService.paginaAlunos();
     }
 
     @GetMapping("/novo")
     public ModelAndView novo(AlunoDTO requisicao) {
-        ModelAndView mv = new ModelAndView("alunos/novo");
-        mv.addObject("requisicao", new AlunoDTO()); // <-- linha essencial
-        mv.addObject("page", "alunos/novo");
-        return mv;
+
+        return alunoService.novoAluno(requisicao);
     }
 
 
     @PostMapping("")
     public ModelAndView create(@Valid AlunoDTO requisicao, BindingResult bindingResult){
 
-        if(bindingResult.hasErrors()){
-            ModelAndView mv = new ModelAndView("alunos/novo");
-            mv.addObject("page", "alunos/novo");
-            mv.addObject("requisicao", requisicao);
-            return mv;
-        } else {
-            Aluno aluno = requisicao.toAluno();
-            this.alunoRepository.save(aluno);
-            return new ModelAndView("redirect:/alunos");
-        }
+        return alunoService.criarnovoAluno(requisicao, bindingResult);
     }
 
     @GetMapping("/{id}")
     public ModelAndView show(@PathVariable Long id) {
 
-        Optional<Aluno> optional = this.alunoRepository.findById(id);
-
-        if (optional.isPresent()) {
-            Aluno alunos = optional.get();
-            ModelAndView mv = new ModelAndView("alunos/show");
-            mv.addObject("page", "alunos/show");
-            mv.addObject("alunos", alunos);
-            return mv;
-        } else {
-            return this.retornaErroAlunos("Não foi possível encontrar o aluno #" + id +"!");
-        }
+        return alunoService.mostrarAluno(id);
     }
 
     @GetMapping("/{id}/edit")
     public ModelAndView edit(@PathVariable Long id, AlunoDTO requisicao) {
-        Optional<Aluno> optional = this.alunoRepository.findById(id);
-
-        if (optional.isPresent()){
-            Aluno aluno = optional.get();
-            requisicao.fromAluno(aluno);
-            ModelAndView mv = new ModelAndView("alunos/edit");
-            mv.addObject("page","alunos/edit");
-            mv.addObject("requisicao", requisicao);
-            mv.addObject("alunoId", aluno.getId());
-            return mv;
-        } else {
-            return this.retornaErroAlunos("Não foi possivel encontrar o aluno #" + id +"!");
-        }
+        return alunoService.editarAluno(id, requisicao);
     }
 
     @PostMapping("/{id}")
     public ModelAndView update(@PathVariable Long id, @Valid AlunoDTO requisicao, BindingResult bindingResult) {
-        if (bindingResult.hasErrors()) {
-            ModelAndView mv = new ModelAndView("alunos/edit");
-            mv.addObject("alunoId", id);
-            mv.addObject("page", "alunos/edit");
-            mv.addObject("requisicao", requisicao); // necessário para mostrar os erros no formulário
-            return mv;
-        } else {
-            Optional<Aluno> optional = this.alunoRepository.findById(id);
-            if (optional.isPresent()) {
-                Aluno aluno = requisicao.toAluno(optional.get());
-                this.alunoRepository.save(aluno);
-                ModelAndView mv = new ModelAndView("redirect:/alunos/" + aluno.getId());
-                mv.addObject("page", "alunos/show");
-                return mv;
-            } else{
-                return this.retornaErroAlunos("Erro de atualização: Não foi possível encontrar o aluno #" + id +"!");
-            }
-        }
+        return alunoService.atualizarAluno(id, requisicao, bindingResult);
     }
 
     @GetMapping("/{id}/delete")
     public ModelAndView delete(@PathVariable Long id) {
-        ModelAndView mv = new ModelAndView("redirect:/alunos");
-        try {
-            this.alunoRepository.deleteById(id);
-            mv.addObject("mensagem", "Veiculo #" + id + " deletado com sucesso!");
-            mv.addObject("erro", false);
-        } catch (EmptyResultDataAccessException e) {
-            System.out.println(e);
-            mv = this.retornaErroAlunos("Erro de delete: Não foi possível encontrar o veiculo #" + id +"!");
-        }
-        return mv;
-    }
-
-
-    private ModelAndView retornaErroAlunos(String mensagem) {
-        ModelAndView mv = new ModelAndView("redirect:/alunos");
-        mv.addObject("mensagem", mensagem);
-        mv.addObject("erro", true);
-        return mv;
+        return alunoService.deletarAluno(id);
     }
 
 }
